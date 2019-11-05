8809be27d530461dbbddae0a2f3a84f0a603badc
EC2 Default User <ec2-user@ip-172-31-36-41.ap-northeast-2.compute.internal>
2019-11-05 13:43:19 +0000

    [Feat] trabis-ci, deploy script 작성

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
-
-REPOSITORY="/home/ec2-user/app/git/AlgorithmWebService"
+APPLICATION="AlgorithmWebService"
+REPOSITORY="/home/ec2-user/app/git/$APPLICATION"
 FILE_NAME="src/main/resources/application.properties src/test/java/com/algorithm/mySQLConnectionTest.java"
-DATABASE_USERNAME="@@USERNAME@@"
-DATABASE_PASSWORD="@@PASSWORD@@"
-DATABASE_SERVER="@@SERVER@@"
 
 cd $REPOSITORY
-echo "> Git Pull"
+echo "Git Pull"
 git pull
-echo "> 프로젝트 Build 시작"
+echo "프로젝트 Build 시작"
 sudo ./mvnw clean package
 
-echo "> 파라미터값 수정"
+echo "구동 중인 appilcation의 pid 확인"
+CURRENT_PID=$(pgrep -f $APPLICATION)
+
+echo "$CURRENT_PID"
+if [ -z $CURRENT_PID ]; then
+	echo ">현재 구동 중인 어플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo ">kill -9 $CURRENT_PID"
+	sudo kill -9 $CURRENT_PID
+fi
+
+echo "파라미터값 수정"
 FILE_NAME="src/main/resources/application.properties src/test/java/com/algorithm/mySQLConnectionTest.java"
 sed -i "s/@@@DATABASE_USERNAME@@@/$DATABASE_USERNAME/g" $FILE_NAME
 sed -i "s/@@@DATABASE_PASSWORD@@@/$DATABASE_PASSWORD/g" $FILE_NAME
 sed -i "s/@@@DATABASE_SERVER@@@/$DATABASE_SERVER/g" $FILE_NAME
 
-echo "> spring boot 시작"
-sudo nohup ./mvnw spring-boot:run
+echo "새 어플리케이션 배포"
+JAR_NAME=$(find target -name "*.jar")
 
-#cp ./build/libs/*.jar $REPOSITORY/
-#echo "> 현재 구동중인 애플리케이션 pid 확인"
-#CURRENT_PID=$(pgrep -f springboot-webservice)
-#echo "$CURRENT_PID"
-#if [ -z $CURRENT_PID ]; then
-#    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-#else
-#    echo "> kill -2 $CURRENT_PID"
-#    kill -9 $CURRENT_PID
-#    sleep 5
-#fi
-#echo "> 새 어플리케이션 배포"
-#JAR_NAME=$(ls $REPOSITORY/ |grep 'springboot-webservice' | tail -n 1)
-#echo "> JAR Name: $JAR_NAME"
-#nohup java -jar $REPOSITORY/$JAR_NAME &
+echo "spring boot 시작"
+sudo nohup java -jar $REPOSITORY/$JAR_NAME &
